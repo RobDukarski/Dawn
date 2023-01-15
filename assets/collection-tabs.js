6ff83b5aa80fd6496eb0379b3175bea1cef90577
Rob Dukarski <RobDukarski@users.noreply.github.com>
2023-01-15 09:49:03 -0500

    Update collection tabs
    Updated the collection tabs functionality.

--- assets/collection-tabs.js
+++ assets/collection-tabs.js
@@ -10,23 +10,30 @@ if (!customElements.get('tab-bar')) {
       super();
 
       this.allLinks = [];
-      this.collectionContentSelectors = {
-        "titleAndDescription": ".content-for-layout .collection-hero__text-wrapper",
-        "tabBar": ".content-for-layout .collection-tabs",
-        "products": ".content-for-layout .product-grid-container"
-      };
+      this.collectionContentSelectors = Object.values({
+        products: '.content-for-layout .shopify-section > [class*="__product-grid"]',
+        tabBar: '.content-for-layout .collection-tabs .tab-bar',
+        titleAndDescription: '.content-for-layout .collection-hero__text-wrapper'
+      });
       this.links = this.querySelectorAll('a');
       this.select = this.querySelector('select');
+      this.state = {
+        title: document.title,
+        url: this.dataset.collectionUrl
+      };
+
+      window.addEventListener('pageshow', this.onHistoryChangeHandler.bind(this));
 
       // Grab all matching links on the page to let them benefit from the
-      // non-reloading functionality too.
+      // non-reloading functionality too
 
       if (!!(this.links) && this.links.length > 0) {
         this.links.forEach((link) => {
-          this.allLinks.concat([...document.querySelectorAll(`a[href*="${link.href}]`)]);
+          this.allLinks = this.allLinks.concat([...document.querySelectorAll(`a[href*="${link.getAttribute('href')}"]`)]);
         });
 
         this.allLinks.forEach((link) => {
+          link.removeEventListener('click', this.onLinkClickHandler.bind(this));
           link.addEventListener('click', this.onLinkClickHandler.bind(this));
         });
       }
@@ -43,36 +50,66 @@ if (!customElements.get('tab-bar')) {
         ?.querySelector(selector)?.innerHTML;
     }
 
+    // Checks the state of the tab bar component to see if a collection should load
+    onHistoryChangeHandler() {
+      if (location.href.indexOf(this.state.url) == -1) {
+        this.switchCollection(this.state.url, false);
+      }
+    }
+
     onLinkClickHandler(event) {
       event.preventDefault();
+      event.stopImmediatePropagation();
 
       const link = event.currentTarget;
 
       if (link.href.indexOf(this.dataset.collectionHandle) > -1) {
         // Do nothing, already on collection page
       } else {
-        this.switchCollection(link.href);
+        this.switchCollection(link.getAttribute('href'), true);
       }
     }
 
     onSelectChangeHandler(event) {
-      const select = event.currentTarget;
+      this.switchCollection(event.currentTarget.value, true);
+    }
 
-      this.switchCollection(select.value);
+    setLoadingState(loading) {
+      if (loading) {
+        document.getElementById('ProductGridContainer')?.querySelector('.collection')?.classList.add('loading');
+      } else {
+        document.getElementById('ProductGridContainer')?.querySelector('.collection')?.classList.remove('loading');
+      }
     }
 
     // Updates the content on the collection page and replaces the current URL
-    switchCollection(url) {
+    switchCollection(url, pushState) {
+      this.setLoadingState(true);
+
       fetch(url)
         .then((response) => response.text())
         .then((data) => {
-          Object.values(this.collectionContentSelectors).forEach((selector) => {
+          this.collectionContentSelectors.forEach((selector) => {
             document.querySelector(selector).innerHTML = this.getContentFromHTML(data, selector);
           });
 
-          history.pushState({}, '', url);
+          this.state = {
+            title: this.getContentFromHTML(data, 'title')?.replace(/\n/g, '').trim(),
+            url
+          };
+
+          if (pushState) {
+            history.pushState(this.state, this.state.title, this.state.url);
+          }
+
+          document.title = this.state.title;
+
+          this.setLoadingState(false);
         })
-        .catch((error) => console.error(error));
+        .catch((error) => {
+          console.error(error)
+          this.setLoadingState(false);
+        });
     }
   });
 }
